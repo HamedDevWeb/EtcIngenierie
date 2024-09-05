7d12b6f9d4b6bbe8493ac4e898d7dd1724e6aaeb
Hamed ATMANE <Ahmedgithub@gmx.fr>
2024-09-05 08:50:22 +0200

    delete

--- src/app/about/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AboutPage = () => {
-  return (
-    <div>Page à Propos</div>
-  )
-}
-
-export default AboutPage
--- src/app/blog/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BlogPage = () => {
-  return (
-    <div>Page de blog</div>
-  )
-}
-
-export default BlogPage
