8ef2027660a8f251475a0f80eba538001a1c0d1a
Hamed ATMANE <Ahmedgithub@gmx.fr>
2024-10-01 14:23:58 +0200

    Elaboration du menu responsive ainsi que l'implémentation css

--- front/src/components/navbar/links/Links.jsx
+++ front/src/components/navbar/links/Links.jsx
@@ -1,43 +1,55 @@
-import styles from './links.module.css'
-import NavLink from './navLink/navLink'
+"use client";
+import { useState } from "react";
+import NavLink from "./navLink/navLink";
+import styles from "./links.module.css";
+
+const links = [
+  {
+    title: "Accueil",
+    link: "/",
+  },
+  {
+    title: "Nos prestations",
+    link: "/services",
+  },
+  {
+    title: "Blog",
+    link: "/blog",
+  },
+  {
+    title: "Nos Valeurs",
+    link: "/values",
+  },
+  {
+    title: "A propos",
+    link: "/about",
+  },
+  {
+    title: "Contact",
+    link: "/contact",
+  },
+];
+
 const Links = () => {
+  const [open, setOpen] = useState(false);
 
-    const links = [
-        {
-            title: 'Accueil',
-            link: '/',
-        },
-        {
-            title: 'Nos prestations',
-            link: '/services',
-        },
-        {
-            title: 'Blog',
-            link: '/blog',
-        },
-        {
-            title: 'Nos Valeurs',
-            link: '/values',
-        },
-        {
-            title: 'A propos',
-            link: '/about',
-        },
-        {
-            title: 'Contact',
-            link: '/contact',
-        },
-    ]
-
-    return (
-        <div className="flex text-xs items-center gap-8 uppercase text-primary">
-            {
-                links.map((link => (
-                    <NavLink item={link} key={link.title} />
-                )))
-            }
+  return (
+    <div>
+      <div className='flex text-xs items-center gap-8 uppercase text-primary'>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+      </div>
+      <button className={styles.menuButton} onClick={() => setOpen((prev) => !prev)}>Menu</button>
+      {
+        open && <div className={styles.mobileLinks}>
+            {links.map((link) => (
+              <NavLink item={link} key={link.title} />
+            ))}
         </div>
-    )
-}
+      }
+    </div>
+  );
+};
 
-export default Links
+export default Links;
